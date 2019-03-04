bfdffe17ff4136ebfc84677cac5a31b6aa652725
Ryan Lin <gogopowerryan@gmail.com>
2019-03-04 15:28:44 +0800

    commit third

--- PythonLearning.py
+++ PythonLearning.py
@@ -2,6 +2,12 @@
 def helloPython(input):
     output = "Hello "+input+" !"
     print(output)
+    x = [1, 2, 3]
+    y = [4, 5, 6]
+    zipped = zip(x, y)
+    print(list(zipped))
+    x2, y2 = zip(*zip(x, y))
+    print(x2+y2)
 
 
 helloPython("Ryan")
