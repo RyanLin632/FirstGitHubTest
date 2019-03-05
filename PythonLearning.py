a67a9fb1fee57352fe2b447a1a22ecca3dcc4a16
Ryan Lin <gogopowerryan@gmail.com>
2019-03-05 10:19:49 +0800

    add math import

--- PythonLearning.py
+++ PythonLearning.py
@@ -1,3 +1,6 @@
+import xmath
+import xmath as math
+
 
 def helloPython(input):
     output = "Hello "+input+" !"
@@ -8,6 +11,15 @@ def helloPython(input):
     print(list(zipped))
     x2, y2 = zip(*zip(x, y))
     print(x2+y2)
+    # --------
+    print(xmath.pi)
+    max = xmath.max(10, 5)
+    min = xmath.min(4, 10)
+    sum = xmath.sum(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    print(max)
+    print(min)
+    print(sum)
+    print(math.e)
 
 
 helloPython("Ryan")
